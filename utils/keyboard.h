02d68b5442d5484e096af0f3ea4bbea6300a4dcc
Yuchan Han <serltretu24@gmail.com>
2024-06-24 11:46:13 +0900

    feat: 키보드 입력

---
+++ utils/keyboard.h
@@ -0,0 +1,11 @@
+#define KEY_GROUP_ARROW 224 // 화살표키
+
+#define KEY_ENTER 13 // 엔터
+#define KEY_LEFT 75 // 좌
+#define KEY_RIGHT 77 // 우
+#define KEY_UP 72 // 상
+#define KEY_DOWN 80 // 하
+#define KEY_SPACE 32 // SPACE
+#define KEY_p 112 // 소문자 p
+#define KEY_P 80 // 대문자 P
+#define KEY_ESC 27 // ESC
