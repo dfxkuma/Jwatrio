3626f3ef5af50d5cd333a08fd620a5ad3ef14232
Yuchan Han <serltretu24@gmail.com>
2024-06-26 18:39:38 +0900

    feat: typehint 추가

---
+++ utils/typehint.h
@@ -0,0 +1,9 @@
+#define TRUE 1
+#define FALSE 0
+
+#define ACTIVE_BLOCK -2 // 게임판배열에 저장될 블록의 상태들
+#define CEILLING -1     // 블록이 이동할 수 있는 공간은 0 또는 음의 정수료 표현
+#define EMPTY 0         // 블록이 이동할 수 없는 공간은 양수로 표현
+#define WALL 1
+#define BOTTOM_WALL 3
+#define INACTIVE_BLOCK 2 // 이동이 완료된 블록값
