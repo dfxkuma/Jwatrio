998dfd5c630bbb38fbb6494aff9dc906633b60cf
Yuchan Han <serltretu24@gmail.com>
2024-06-26 18:40:38 +0900

    feat: 테트리스 엔진 event

---
+++ game/event.c
@@ -0,0 +1,11 @@
+int onGameBlockChange(int x, int y, int blockType, int blockStatus);
+
+int onGamePause(int status);
+
+int onGameLineCrash(int line);
+
+int onGameOver();
+
+int onNetworkConnect();
+
+int onNetworkDisconnect();
