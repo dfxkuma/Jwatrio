1b69bd66e2da4c52f355a74185e28b015a039e62
Yuchan Han <serltretu24@gmail.com>
2024-06-26 18:40:11 +0900

    feat: multiplay scene 추가

--- screen.h
+++ screen.h
@@ -3,3 +3,4 @@ int screenStartHome(int screenX, int screenY);
 int screenStartSoloGame(int screenX, int screenY);
 int screenStartGame(int screenX, int screenY);
 int screenStartConnectServer(int screenX, int screenY);
+int screenStartMultiplay(int screenX, int screenY, char* ip, char* roomName, int playerCount);
